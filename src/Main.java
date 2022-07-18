6f75456ac5e4f0292e0607d64e91274b0f505cff
Andrey Beloboky <andrei.beloboky@yandex.ru>
2022-07-19 00:02:37 +0300

    add a solution to the problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-       
+        List<String> strings = new LinkedList<>();
+        strings.add("мама");
+        strings.add("мыла");
+        strings.add("раму");
+        strings.forEach(Main::staticMethod);
+        Main m = new Main();
+        strings.forEach(m::method);
+
+        List<User> users = new LinkedList<>();
+        users.add(new User("Вася"));
+        users.add(new User("Коля"));
+        users.add(new User("Петя"));
+
+        users.forEach(User::print);
+    }
+
+    public void method(String s) {
+
+    }
+
+    public static void staticMethod(String s) {
+
     }
 }
---
+++ src/User.java
@@ -0,0 +1,11 @@
+public class User {
+    private String name;
+
+    User(String name) {
+        this.name = name;
+    }
+
+    void print() {
+        System.out.println(name);
+    }
+}
